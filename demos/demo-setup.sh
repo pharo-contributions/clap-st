c731e0b2ae05894f51cc2a260e4b609e19ffc2ae
Damien Pollet <damien.pollet@gmail.com>
2018-08-28 17:46:10 +0200

    Demo animation

---
+++ demos/demo-setup.sh
@@ -0,0 +1,11 @@
+# -*- mode: sh; -*-
+
+alias clap='~/bin/pharo-vms/64/pharo clap.00d2cc5.image clap'
+
+function :D {
+    print
+    print "\e[37m                                Clap, the command-line argument parser for Pharo\e[0m"
+    print "\e[34m                                                 https://github.com/cdlm/clap-st\e[0m"
+    exit
+clear
+}
---
+++ demos/record.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+asciinema rec \
+    --title 'Clap: command line arguments parsing in Pharo' \
+    --idle-time-limit 4 \
+    --overwrite "${1:-wip.cast}"
